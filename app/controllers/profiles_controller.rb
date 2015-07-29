10fc33fd51a7afde877cacef50114aa61e98f103
GusMilner <gusmilner@gmail.com>
2015-07-29 12:49:04 -0400

    Added profiles view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,9 @@
 class ProfilesController < ApplicationController
-
+	def new
+		# form where user fills out own profile
+		@user = User.find( params[:user_id])
+		@variable = params[:hello]
+		@profile = @user.build_profile
+	end
 
 end
