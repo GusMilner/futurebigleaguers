6c3e7ad83f69580c0af669ee719116b4c43fad51
GusMilner <gusmilner@gmail.com>
2015-08-01 22:38:39 -0400

    added security features to users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+	before_action :authenticate_user!
+  	before_action :only_current_user
 	def new
 		# form where user fills out own profile
 		@user = User.find( params[:user_id])
@@ -22,10 +24,28 @@ class ProfilesController < ApplicationController
 		@profile = @user.profile
 	end
 
+
+	def update
+		@user = User.find( params[:user_id])
+		@profile = @user.profile
+		if @profile.update_attributes(profile_params)
+			flash[:success] = "Profle Updated!"
+			redirect_to user_path(params[:user_id])
+		else
+			render acton: :edit
+		end
+
+	end
+
 	private
 
-	def profile_params
-		params.require(:profile).permit(:first_name, :last_name, :position, :grade, :team, :phone_number, :contact_email, :description)
-	end
+		def profile_params
+			params.require(:profile).permit(:first_name, :last_name, :position, :grade, :team, :phone_number, :contact_email, :description)
+		end
+
+		def only_current_user
+	      @user = User.find( params[:user_id] )
+	      redirect_to(root_url) unless @user == current_user
+	    end
 
 end
