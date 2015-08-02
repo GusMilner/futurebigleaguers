02c797398dc6427bccfe9badaba33205ad8256ca
GusMilner <gusmilner@gmail.com>
2015-08-01 22:22:32 -0400

    added edit and form partial for user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,9 +15,11 @@ class ProfilesController < ApplicationController
 			render acton: :new
 		end
 
+	end
 
-
-
+	def edit
+		@user = User.find( params[:user_id])
+		@profile = @user.profile
 	end
 
 	private
